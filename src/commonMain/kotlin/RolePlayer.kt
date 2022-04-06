1792bb4658383181fc62697b7d6c795c245537db
Jawa350T12 <91010789+Jawa350T12@users.noreply.github.com>
2022-04-06 19:29:40 +0300

    Initial commit

---
+++ src/commonMain/kotlin/RolePlayer.kt
@@ -0,0 +1,3 @@
+class RolePlayer constructor(side:String,nameRole:String,hp:Int,speed:Int,weapon: Weapon,gear:String) {
+
+}
---
+++ src/commonMain/kotlin/Weapon.kt
@@ -0,0 +1,3 @@
+open class Weapon constructor(nameWeapon:String, rangeDistance:Int, moduls:String, sizeMagazine:Int, damage:Int) {
+
+}
